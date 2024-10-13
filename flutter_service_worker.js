b258b347432700e8424c33d0cc3113e801d8c647
kolianych <98098158+kolianych@users.noreply.github.com>
2024-10-13 21:17:10 +0300

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -106,7 +106,7 @@ const CORE = ["main.dart.js",
 "index.html",
 "flutter_bootstrap.js",
 "assets/AssetManifest.bin.json",
-"assets/FontManifest.json"];
+"test_image_loading.io/assets/FontManifest.json"];
 
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
